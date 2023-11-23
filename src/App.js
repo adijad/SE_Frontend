187fe0a6a85e3133f18e948f31eb946a0c1fda1c
sushmanukala9 <sushmashivani@vt.edu>
2023-11-23 17:55:47 -0500

    visual studio code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter,Routes,Route } from "react-router-dom";
+import Register from "./components/Register";
+import Login from "./components/Login";
+ 
+import Home from "./components/Home";
+import {useParams} from "react-router-dom";
+import {useEffect, useState} from "react";
 
 function App() {
+  const {catId} = useParams();
+  useEffect(()=>{
+    console.log(catId)},[catId]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
+      <BrowserRouter>
+            <Routes>
+              <Route path="/home" element= { <Home/>} />
+              <Route path="/register" element= { <Register/>} />
+              <Route path="/login" element= { <Login/>} />
+            </Routes>
+        </BrowserRouter>
+      
     </div>
   );
 }
